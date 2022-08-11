d3fc37b3f347634c656fd0eb0b53f2164be4ad9d
jiangjiang913 <1243554752@qq.com>
2022-08-11 18:44:23 +0800

    修改

--- src/main/java/org/example/olap_web/dao/AdvertiseDao.java
+++
@@ -1,9 +0,0 @@
-package org.example.olap_web.dao;
-
-import org.example.olap_web.pojo.Advertise;
-
-import java.util.List;
-
-public interface AdvertiseDao {
-    List<Advertise> getAdvertiseInfo();
-}
